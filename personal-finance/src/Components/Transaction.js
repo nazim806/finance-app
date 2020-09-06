c96ad8d87a022042cc233546328238d208b1c7e1
Nazim Uddin <umnazim@gmail.com>
2020-09-05 23:56:44 -0700

    transaction adjusted

--- personal-finance/src/Components/Transaction.js
+++ personal-finance/src/Components/Transaction.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <li className="negative">
-      {transaction.text} <span>-$400</span>
+      {transaction.text}{" "}
+      <span>
+        {sign}${transaction.amount}
+      </span>
       <button className="delete-btn">X</button>
     </li>
   );
