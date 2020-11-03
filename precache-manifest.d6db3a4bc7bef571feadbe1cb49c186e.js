6fd74116135fa233859b457bdaf8815047c439ad
Nazim Uddin <umnazim@gmail.com>
2020-11-02 19:02:21 -0800

    Updates

---
+++ precache-manifest.d6db3a4bc7bef571feadbe1cb49c186e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256a6951c8b62756060d5b5245e4c1ac",
+    "url": "/finance-app/index.html"
+  },
+  {
+    "revision": "cfc243ef8ac1f64a16cc",
+    "url": "/finance-app/static/css/main.29f25a57.chunk.css"
+  },
+  {
+    "revision": "5f4b4cdadfcac08a7db7",
+    "url": "/finance-app/static/js/2.feb679f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/finance-app/static/js/2.feb679f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfc243ef8ac1f64a16cc",
+    "url": "/finance-app/static/js/main.e6a0dc1a.chunk.js"
+  },
+  {
+    "revision": "be3a0471e27351d9bdff",
+    "url": "/finance-app/static/js/runtime-main.795ddc1e.js"
+  }
+]);
